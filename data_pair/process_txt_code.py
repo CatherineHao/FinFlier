b9f3a27843f43d76ff40e6848b748886307ed6ba
YuzheJiang <345854890@qq.com>
2023-08-14 10:22:12 +0800

    update data

--- data_pair/process_txt_code.py
+++ data_pair/process_txt_code.py
@@ -3,7 +3,7 @@ import csv
 
 data  = []
 
-file = pd.read_csv('./7_data_rounding_bottom.csv')
+file = pd.read_csv('./used_data/20_data.csv')
 # print(file.head())
 file = file.to_dict(orient='records')
 print(file)
