36ae633bd449ebdc067f488aa46d5a24d8466f6d
YuzheJiang <345854890@qq.com>
2023-09-04 16:55:53 +0800

    update pattern prompt

--- data_pair/process_txt_code.py
+++ data_pair/process_txt_code.py
@@ -6,7 +6,7 @@ data  = []
 
 pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
-file = pd.read_csv('./used_data/24_multibar.csv')
+file = pd.read_csv('./used_data/6_data_cup_with_handle.csv')
 # print(file.head())
 file = file.to_dict(orient='records')
 # with open("txt_7.txt","w") as f:
